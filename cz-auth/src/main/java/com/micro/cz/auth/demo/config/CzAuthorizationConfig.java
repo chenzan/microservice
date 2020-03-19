ad86dbe931b1d2ffbb92c1278ac99e2fab7093c2
chenzan <chenzan@hxtech-bj.com>
2020-03-19 11:27:57 +0800

    提交修改

--- cz-auth/src/main/java/com/micro/cz/auth/demo/config/CzAuthorizationConfig.java
+++ cz-auth/src/main/java/com/micro/cz/auth/demo/config/CzAuthorizationConfig.java
@@ -1,9 +1,17 @@
 package com.micro.cz.auth.demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+
+import java.util.Arrays;
 
 /**
  * @author chenzan
@@ -14,8 +22,11 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
  * @modifier-data
  */
 @Configuration
+@Order(Integer.MIN_VALUE)
 @EnableAuthorizationServer
 public class CzAuthorizationConfig extends AuthorizationServerConfigurerAdapter {
+    @Autowired
+    private AuthenticationManager authenticationManager;
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory()
@@ -23,5 +34,17 @@ public class CzAuthorizationConfig extends AuthorizationServerConfigurerAdapter 
                 .secret("123456")
                 .authorizedGrantTypes("up")
                 .scopes("auth");
+    }
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+        //token增强配置
+        TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
+        InMemoryTokenStore imt = new InMemoryTokenStore();
+        endpoints
+                .tokenStore(imt)
+                .tokenEnhancer(tokenEnhancerChain)
+                .authenticationManager(authenticationManager)
+                .reuseRefreshTokens(false);
+//                .userDetailsService(userDetailsService);
     }
 }
--- cz-gateway/src/main/java/com/example/gateway/CzGatewayApplication.java
+++ cz-gateway/src/main/java/com/example/gateway/CzGatewayApplication.java
@@ -5,14 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-import java.awt.*;
-
 @EnableZuulProxy
 @EnableEurekaClient
 @SpringBootApplication
 public class CzGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CzGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CzGatewayApplication.class, args);
+    }
 }
